b11c0e8fe991862c20171701245a385aa4086365
Gabriel Nieves Ponce <gabrielnieves18@gmail.com>
2017-12-06 12:38:44 -0500

    s

--- project/icas/images/models.py
+++ project/icas/images/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Image(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, blank=True, default='')
+#    created = models.DateTimeField(auto_now_add=True)
+#    title = models.CharField(max_length=100, blank=True, default='')
     image = models.ImageField(upload_to='media', default='media/out/out.jpg')
     
     class Meta:

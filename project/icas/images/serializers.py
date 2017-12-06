516925db5d78061fb9d58f93f21747ce8da3c169
Gabriel Nieves Ponce <gabrielnieves18@gmail.com>
2017-12-06 13:29:37 -0500

    .

--- project/icas/images/serializers.py
+++ project/icas/images/serializers.py
@@ -5,16 +5,8 @@ from model import Model
 
 
 class ImageSerializer(serializers.ModelSerializer):
-
     image = serializers.ImageField(max_length=None, use_url=True)
-    print('\n\nThe image = {}\n\n'.format(image))
-	
-    modl = Model()
-	
-    objects = modl.inferense(image_uri=image)
-    
-    print(objects)	
-    
+
     class Meta:
         model = Image
         fields = ('id',
--- project/icas/images/views.py
+++ project/icas/images/views.py
@@ -2,10 +2,26 @@ from images.models import Image
 from rest_framework import viewsets
 from images.serializers import ImageSerializer
 
+from model import Model
+
 class ImageViewSet(viewsets.ModelViewSet):
     serializer_class = ImageSerializer
 
     def get_queryset(self):
         return Image.objects.all().order_by('created')
 
-
+	def perform_create(self, serializer):
+    
+        uri = serializer.validated_data['image']
+        serializer.validated_data['password'] = hashed_password 
+        image = super(ImageViewSet, self).perform_create(serializer) # create a user
+        
+        print('\n\nThe image = {}\n\n'.format(uri))
+	
+        modl = Model()
+	
+        objects = modl.inferense(image_uri=uri)
+    
+        print(objects)	
+    
+	

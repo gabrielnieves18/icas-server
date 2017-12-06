d3acc855f6dd7a5cd29959f69df7616f1b3f0972
Gabriel Nieves Ponce <gabrielnieves18@gmail.com>
2017-12-06 13:18:55 -0500

    .

--- project/icas/images/serializers.py
+++ project/icas/images/serializers.py
@@ -7,7 +7,7 @@ from model import Model
 class ImageSerializer(serializers.ModelSerializer):
 
     image = serializers.ImageField(max_length=None, use_url=True)
-	print('\n\nThe image = {}\n\n'.format(image))
+    print('\n\nThe image = {}\n\n'.format(image))
 	
     modl = Model()
 	

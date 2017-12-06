e3919ca811f672dbbadafde00ab60c2b1e0bfbc9
Gabriel Nieves Ponce <gabrielnieves18@gmail.com>
2017-12-06 13:17:52 -0500

    .

--- project/icas/images/serializers.py
+++ project/icas/images/serializers.py
@@ -7,13 +7,14 @@ from model import Model
 class ImageSerializer(serializers.ModelSerializer):
 
     image = serializers.ImageField(max_length=None, use_url=True)
+	print('\n\nThe image = {}\n\n'.format(image))
 	
     modl = Model()
 	
     objects = modl.inferense(image_uri=image)
     
-    print(objects)
-    	
+    print(objects)	
+    
     class Meta:
         model = Image
         fields = ('id',
